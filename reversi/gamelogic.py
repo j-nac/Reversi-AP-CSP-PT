1664b3db6bcd1f2a20ea2486c2f7c4d9a6da75c0
Jakob Nacanaynay <63821647+j-nac@users.noreply.github.com>
2022-04-08 12:06:00 -0700

    Add function to find flips

--- reversi/gamelogic.py
+++ reversi/gamelogic.py
@@ -28,16 +28,64 @@ class Board:
         pass
 
     def get_legal_positions(self, move):
-        # For every 0, if up, down, left, or right is opp color, go down the row find same color
-        pass
 
-    def move(self, move):
+        # Start N, go clockwise
+        directions = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+
+        for row in self.board:
+            for square in row:
+                if square == 1 or square == 2:
+                    pass
+    
+
+    def check_move_flips(self, coords, color_num):
+        flips = []
+        directions = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+
+        if not(color_num == 1 or color_num == 2):
+            raise ValueError('color_num must be 1 or 2')
+        
+        opp_color = ((color_num - 1.5) * -2 + 3) / 2
+
+        for d in directions:
+            test_coords = tuple(map(sum, zip(coords, d)))
+            print(test_coords)
+            if self.is_in_bounds(test_coords):
+                test_square = self.board[test_coords[1]][test_coords[0]]
+                # Check if opposite color in between
+                if test_square != opp_color:
+                    continue
+                # Find anchor while saving flips
+                possible_flips = [test_coords]
+                while True:
+                    test_coords = tuple(map(sum, zip(test_coords, d)))
+                    if self.is_in_bounds(test_coords):
+                        test_square = self.board[test_coords[1]][test_coords[0]]
+                        if test_square == opp_color:
+                            possible_flips.append(test_coords)
+                        elif test_square == color_num:
+                            flips += possible_flips
+                            break
+                        else:
+                            break
+                    else:
+                        break
+        
+        return flips
+                    
+    def execute_move(self, coords):
         pass
 
     def update_score(self):
         self.empty_count = sum([row.count(0) for row in self.board])
         self.black_count = sum([row.count(1) for row in self.board])
         self.white_count = sum([row.count(2) for row in self.board])
+    
+    def is_in_bounds(self, coords):
+        for c in coords:
+            if c < 0 or c > 7:
+                return False
+        return True
 
 b = Board()
-print(b.empty_count)
+print(b.check_move_flips((4, 5), 1))
