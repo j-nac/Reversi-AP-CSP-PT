4c98ad3406183ba41ecb70699f084654a22c06bd
Jakob Nacanaynay <63821647+j-nac@users.noreply.github.com>
2022-04-21 20:26:40 -0700

    Finish the project

--- reversi.py
+++ reversi.py
@@ -1,4 +1,6 @@
 import os
+import random
+import time
 
 class Board:
     def __init__(self, srn='8/8/8/3wb3/3bw3/8/8/8 b'):
@@ -27,6 +29,10 @@ class Board:
 
     def get_srn(self):
         pass
+
+    @staticmethod
+    def convert_move_format(move):
+        return ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'][move[0]] + str(move[1] + 1)
 
     def get_legal_moves(self, color_num: int) -> list:
         legal_moves = []
@@ -95,17 +101,19 @@ class Board:
     
     def check_gameover(self):
         if not (self.get_legal_moves(1) or self.get_legal_moves(2)):
+            total = f'Black {self.black_count} - White {self.white_count}'
             if self.black_count > self.white_count:
-                return 'Black side wins'
+                return 'Black side wins\n' + total
             elif self.white_count > self.black_count:
-                return 'White side wins'
+                return 'White side wins\n' + total
             else:
-                return 'Draw'
+                return 'Draw\n' + total
         else:
             return False
 
 def ask_user(question, options):
     while True:
+        os.system('cls')
         print(question)
         for i, option in enumerate(options):
             print(f'{i+1}. {option}')
@@ -116,50 +124,86 @@ def ask_user(question, options):
             else:
                 return int(answer)
         except Exception:
-            input(f'You must enter a number from 1 to {len(options)} as your answer.\nHit ENTER to continue\n')
+            os.system('cls')
+            input(f'You must enter a number from 1 to {len(options)} as your answer.\nHit ENTER to continue')
 
 if __name__ == '__main__':
+    os.system('cls')
     board = Board()
+
+    reversi_ascii = '''
+===================================================
+||\\\\\\\\\\   |||||| \\\\     || ||\\\\\\\\\\   //||||| ||||||
+||   ||   ||      \\\\    || ||   ||   ||        ||
+||   //   ||       \\\\   || ||   //   \\\\|||\\\\   ||
+|||||\\\\   ||        \\\\  || |||||\\\\        ||   ||
+||    \\\\  ||         \\\\ || ||    \\\\       ||   ||
+||     \\\\ ||||||      \\\\|| ||     \\\\ |||||// ||||||
+===================================================
+'''
+
+    print(reversi_ascii)
+    input('\nHit ENTER to continue')
+
+    os.system('cls')
 
     mode = ask_user('Select a game mode', ['Single player', 'Two players'])
 
     side = 1
     while True:
-        print(board)
-
         is_gameover = board.check_gameover()
         if is_gameover:
             print(is_gameover)
             break
 
-        ans = ask_user('What do you want to do?', ['Move', 'Get legal moves', 'Give up'])
+        if len(board.get_legal_moves(side)) == 0:
+            input('There are no legal moves that can be performed this turn\nHit ENTER to continue\n')
+            continue
+
+        ans = ask_user(str(board) + ('\nBlack side\'s turn' if side == 1 else '\nWhite side\'s turn') + '\nWhat do you want to do?', ['Move', 'Get legal moves', 'Give up'])
 
         if ans == 1:
             while True:
                 try:
+                    os.system('cls')
+                    print(board)
                     raw_move = input('What is your move? Format as XY (Ex. C7)\n> ')
                     if len(raw_move) != 2:
                         raise
-                    if raw_move[1] not in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']:
+                    if raw_move[0] not in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']:
                         raise
-                    if int(raw_move[2]) not in [1, 2, 3, 4, 5, 6, 7, 8]:
+                    if int(raw_move[1]) not in [1, 2, 3, 4, 5, 6, 7, 8]:
                         raise
-                    move = (['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'].index(raw_move[0]), int(raw_move[1]))
+                    move = (['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'].index(raw_move[0]), int(raw_move[1]) - 1)
 
-                    if move not in board.get_legal_moves():
+                    if move not in board.get_legal_moves(side):
                         raise
                     else:
                         break
                 except Exception:
-                    input(f'You must enter a legal move in the form XY as your answer. (Ex. G2)\nHit ENTER to continue\n')
+                    os.system('cls')
+                    input('You must enter a legal move in the form XY as your answer. (Ex. G2)\nHit ENTER to continue')
 
             board.execute_move(move, side)
-            side = round(((side - 1.5) * -2 + 3) / 2)
+            if mode == 1:
+                os.system('cls')
+                print(board)
+                input('Hit ENTER to continue')
+                for x in range(4):
+                    os.system('cls')
+                    print('CPU deciding' + x*'.')
+                    time.sleep(0.5)
+                if len(board.get_legal_moves(2)):
+                    board.execute_move(random.choice(board.get_legal_moves(2)), 2)
+                else:
+                    continue
+            else:
+                side = 1 if side == 2 else 2
         elif ans == 2:
-            print(board.get_legal_moves(side))
+            os.system('cls')
+            print(board)
+            print(', '.join([board.convert_move_format(m) for m in board.get_legal_moves(side)]))
+            input('Hit ENTER to continue')
         elif ans == 3:
-            print('Lol')
+            print('Goodbye')
             break
-            
-        
-        # Switch color: side = round(((side - 1.5) * -2 + 3) / 2)
